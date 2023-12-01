8a8c2f4ce730b1577e68e469e1ebbf0c9a375d5a
hasnan023 <itxhaxnan@gmail.com>
2023-12-01 05:57:11 +0500

    Update navBar.jsx

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,31 +1,21 @@
-
-
-import React from 'react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom'; // If you're using React Router
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PersonIcon from '@mui/icons-material/Person';
 import Person2Icon from '@mui/icons-material/Person2';
 import SearchIcon from '@mui/icons-material/Search';
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//import { Icon } from '@mui/material';
+import { Navbar, Nav, Container, Button, FormControl } from 'react-bootstrap';
 
-
-
-
-function Navbar() {
-
-    const [isSearchVisible, setSearchVisible] = useState(false);
-    const [searchQuery, setSearchQuery] = useState('');
+function NavbarComponent() {
+  const [isSearchVisible, setSearchVisible] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
 
   const handleSearchButtonClick = () => {
     setSearchVisible(!isSearchVisible);
   };
 
   const handleSearch = () => {
-    // Perform search based on the searchQuery
     console.log('Performing search for:', searchQuery);
-    // You can add your search logic here
+    // Add your search logic here
   };
 
   const handleSearchInputChange = (e) => {
@@ -33,61 +23,47 @@ function Navbar() {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
+    <Navbar bg="light" expand="lg">
+      <Container>
         <Link to="/" className="navbar-brand">Kitabein</Link>
-        
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mx-auto">
-            <li className="nav-item">
-              <Link to="/home" className="nav-link">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/products" className="nav-link">Products</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact" className="nav-link">Contact</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-link">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/Addproduct" className="nav-link">Add product</Link>
-            </li>
-            {/* Add more navigation items as needed */}
-          </ul>
-        </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <div className="d-flex align-items-center">
-          <Link to="/cart" className="nav-link">
-          <ShoppingCartIcon/>
-          </Link>
-          <Link to="/profile" className="nav-link-m4" style={{marginLeft:"2rem"}}><Person2Icon/>
-          
-          </Link>
-          
-          <div to="/search" className="nav-link-m4" onClick={handleSearchButtonClick} 
-          
-          style={{marginLeft:"2rem"}}><SearchIcon/>
-          {isSearchVisible && (
-        <>
-          <input
-            type="text"
-            className="search-input"
-            placeholder="Search..."
-            value={searchQuery}
-            onChange={handleSearchInputChange}
-          />
-          <button onClick={handleSearch}>Search</button>
-        </>
-      )}
-    
-          </div>
-        </div>
-      </div>
-    </nav>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/home" className="nav-link">Home</Link>
+            <Link to="/products" className="nav-link">Products</Link>
+            <Link to="/contact" className="nav-link">Contact</Link>
+            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/Addproduct" className="nav-link">Add product</Link>
+          </Nav>
+
+          <Nav className="ms-auto align-items-center">
+            <Link to="/cart" className="nav-link">
+              <ShoppingCartIcon />
+            </Link>
+            <Link to="/profile" className="nav-link">
+              <Person2Icon />
+            </Link>
+            <div className="nav-link" onClick={handleSearchButtonClick}>
+              <SearchIcon />
+              {isSearchVisible && (
+                <>
+                  <FormControl
+                    type="text"
+                    className="search-input"
+                    placeholder="Search..."
+                    value={searchQuery}
+                    onChange={handleSearchInputChange}
+                  />
+                  <Button onClick={handleSearch}>Search</Button>
+                </>
+              )}
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
-
+export default NavbarComponent;
